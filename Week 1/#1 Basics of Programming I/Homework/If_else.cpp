69e9ada64900fda2048ab6512c9b83e7c6cf2506
ChetanAnand812 <chetananand812@gmail.com>
2023-02-03 12:00:21 +0530

    initial commit

---
+++ Week 1/#1 Basics of Programming I/Homework/If_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int bronum;
+    cout<<"enter bro num: "<<endl;
+    cin>>bronum;
+
+    if(bronum == 0){
+        cout<<"Batt banjayegi";
+    }
+    else{
+        cout<<"Baat nahi banegi";
+    }
+}
