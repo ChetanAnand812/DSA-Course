40aae6e0802ba3ea5068174e8fb988563b65cb9b
ChetanAnand812 <chetananand812@gmail.com>
2023-02-22 22:31:15 +0530

    initial commit

---
+++ Week 3/2D Array/intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // declare 2D array             c*i+j
+    int arr[3][3];
+
+    // initialize
+    int brr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    cout<<brr[0][2]<<endl;
+
+    return 0;
+}
