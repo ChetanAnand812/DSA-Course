25a60376422deb0eb90f1ca5319a92c85f2c9aa6
ChetanAnand812 <chetananand812@gmail.com>
2023-05-14 11:12:25 +0530

    initial commit

--- Week 3/2D Array/input.cpp
+++ Week 3/2D Array/input.cpp
@@ -15,7 +15,7 @@ int main(){
     }
      
     //output
-    cout<<"Output Array";
+    cout<<"Output Array"<<endl;
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             cout<<arr[i][j]<<" ";
