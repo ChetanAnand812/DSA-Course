1b02e6237a2cf978e9a2d0b8a00a213db7074131
ChetanAnand812 <chetananand812@gmail.com>
2023-07-08 13:27:52 +0530

    initial commit

--- Week 4/Sorting/Bubble_Sort.cpp
+++ Week 4/Sorting/Bubble_Sort.cpp
@@ -15,13 +15,17 @@ int main(){
     // Bubble Sort
     // outer loop -> n rounds
     for(int round=1; round<arr.size(); round++){
-
+        int swapCount = 0;
         for(int j=0; j<arr.size()-round; j++){
 
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                swapCount++;
             }
-
+        }
+        if(swapCount == 0){
+            //sort ho chuka hai, no need to check in further rounds
+            break;
         }
     }
 
